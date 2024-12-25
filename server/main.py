fceb46a36f3fc813669d59838cd18d33cfce134b
William Zhang <william.zhang.ml@gmail.com>
2024-12-25 01:35:13 -0500

    add image scoring route

---
+++ server/main.py
@@ -0,0 +1,49 @@
+"""
+Serverside implementation.
+- assigns quests to users
+- scores user submissions
+- tracks user scores
+"""
+
+__author__ = 'William Zhang'
+
+from typing import Dict, Tuple
+from fastapi import FastAPI
+import numpy as np
+from PIL import Image
+from pydantic import BaseModel
+from . import serverutils
+
+
+app = FastAPI()
+
+
+class RawImage(BaseModel):
+    """Expected image format to receive from client devices. """
+    mode: str              # ex: RGB
+    size: Tuple[int, int]  # width x height
+    data: str
+
+
+@app.post('/score/')
+async def score(raw: RawImage) -> Dict[str, int]:
+    """Score an image (for plumbing checks).
+
+    Args:
+        raw (RawImage): hex-encoded flattened image
+
+    Returns:
+        Dict[str, int]: image score
+    """
+    result = {'score': float('nan')}
+
+    # preprocess image for inference engine
+    data = bytes.fromhex(raw.data)  # undo client conversion to hex/str
+    img = Image.frombytes(raw.mode, raw.size, data)
+    img = np.array(img, dtype=np.float32)
+    img /= 255
+
+    # inference
+    result['score'] = serverutils.score_image(img)
+
+    return result
---
+++ server/serverutils.py
@@ -0,0 +1,18 @@
+"""
+Server utilities for number crunching.
+"""
+__author__ = 'William Zhang'
+
+import numpy as np
+
+
+def score_image(img: np.ndarray) -> int:
+    """Score an image
+
+    Args:
+        img (np.ndarray): image to score
+
+    Returns:
+        float: score
+    """
+    return 100
