2cece523c08223569fbf141212ea08eda25ed356
William Zhang <william.zhang.ml@gmail.com>
2024-12-26 23:53:49 -0500

    add button for asking server for new quest

--- client/main.py
+++ client/main.py
@@ -12,18 +12,20 @@ from kivy.uix.boxlayout import BoxLayout
 class UserInterface(BoxLayout):
     """User interface. """
     _user_data = ObjectProperty(None)
+    _quest_but = ObjectProperty(None)
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self._get_user_data()
 
     def _get_user_data(self) -> None:
-        UrlRequest(
+        req = UrlRequest(
             'http://localhost:8000/user/',
-            on_success=self._render_user_data
+            on_success=self._unpack_user_data
         )
+        req.wait(0)
 
-    def _render_user_data(
+    def _unpack_user_data(
         self,
         req: UrlRequest,
         result: Any
@@ -35,6 +37,16 @@ class UserInterface(BoxLayout):
             result (Any): request result
         """
         self._user_data.text = json.dumps(result)
+        self._quest_but.disabled = result['has_quest'] == 1
+
+    def _get_new_quest(
+        self
+    ) -> None:
+        req = UrlRequest(
+            'http://localhost:8000/newquest/',
+            on_success=self._unpack_user_data
+        )
+        req.wait(0)
 
 
 class ClientApp(App):
